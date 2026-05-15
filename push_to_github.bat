34c384dd751db259c9a0bab7c21a4a3eb926b067
apatino559085-dev <apatino559085-dev@users.noreply.github.com>
2026-05-15 16:25:04 +1200

    first commit

---
+++ push_to_github.bat
@@ -0,0 +1,7 @@
+@echo off
+git add .
+git commit -m "Davao Service Company Project Update"
+git remote set-url origin https://github.com/apatino559085-dev/DAVAO_SERVICE_COMPANY.git
+git push -u origin main
+pause
+
